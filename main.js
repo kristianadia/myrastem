7898d22420b4b63aeab176200f62065454cd6432
cirylrodionoff <liewik@outlook.com>
2023-06-11 14:39:37 +0200

    init taplink project 11.04.2023

--- main.js
+++ main.js
@@ -1,3 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello')
+    const cards = document.querySelectorAll('.card-body');
+    cards.forEach((item) => {
+        const textBlock = item.getElementsByClassName('card-text')[0];
+            console.log(item)
+       if (textBlock.scrollHeight > 300) {
+           const buttonElement = item.getElementsByClassName('show-all')[0];
+
+           textBlock.classList.add('card-text-overflow');
+
+           if (buttonElement) {
+               buttonElement.classList.add('show')
+           }
+
+       }
+    })
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    var cookiePopup = document.getElementById("cookie-popup");
+    var acceptButton = document.getElementById("accept-cookie");
+
+    acceptButton.addEventListener("click", function() {
+        cookiePopup.style.display = "none";
+        setCookie("cookieAccepted", "true", 365); // Установка куки на 365 дней
+    });
+
+    // Проверка, была ли уже принята политика использования файлов cookie
+    var cookieAccepted = getCookie("cookieAccepted");
+    if (!cookieAccepted) {
+        cookiePopup.style.display = "block";
+    }
+});
+
+// Функция для установки куки
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Функция для получения значения куки
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+
+
+
